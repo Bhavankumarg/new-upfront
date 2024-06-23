7d43865bd33f3303a4d51798f0c4aa2214cc8bac
JPivista <jpivistasolutions@gmail.com>
2024-06-23 09:41:05 +0530

    new code is updated

---
+++ config.js
@@ -0,0 +1,10 @@
+const config = {
+    apiDomainUrl: "https://beta.upfront.global/",
+    wpApiUrl: "https://beta.upfront.global/wp-json/wp/v2",
+    wpPageUrl: "",
+    // Career form
+    careeFormApi: "https://docs.ivistaz.com/wp-json/contact-form-7/v1/contact-forms/239/feedback",
+    "SERVER": "8"
+}
+
+export default config;
