4e9fdd24427744c62482232a4ba089b2dbfe1235
Bhavankumarg <gbhavanbkr@gmail.com>
2024-06-29 14:21:16 +0530

    footer changes

--- app/contact-us/page.js
+++ app/contact-us/page.js
@@ -9,15 +9,11 @@ import { useState } from "react";
 import ContactUsAccordion from "@components/ContactUsAccordion";
 import ContactForms from "@utils/ContactForms";
 import PartnerForm from "@utils/PartnerForm";
-import Seo from "../../components/SeoComponents/Seo"
+import Seo from "../../components/SeoComponents/Seo";
 import Link from "next/link";
 
 const Page = () => {
-
-  
-
-
-  const [activeTab, setActiveTab] = useState('Partner With Us');
+  const [activeTab, setActiveTab] = useState("Partner With Us");
 
   const renderContent = () => {
     switch (activeTab) {
@@ -50,25 +46,26 @@ const Page = () => {
               <h2 className="text-black lg:text-6xl text-3xl">CONTACT INFO</h2>
             </div>
 
-           
-              <div className="lg:p-4 pt-7 text-xl" >Office Address:
-              <div className="lg:grid grid-cols-3 gap-4 text-xl border-b border-gray-400">
-              </div>
+            <div className="lg:p-4 pt-7 text-xl">
+              Office Address:
+              <div className="lg:grid grid-cols-3 gap-4 text-xl border-b border-gray-400"></div>
               <div className="mt-3">
-              <div className="text-xl">Bengaluru:</div>
-              <div>#25, 1st Main, AECS Layout, Ashwath Nagar, Bengaluru, 560094</div>
+                <div className="text-xl">Bengaluru:</div>
+                <div>
+                  #25, 1st Main, AECS Layout, Ashwath Nagar, Bengaluru, 560094
+                </div>
               </div>
-
               <div className="mt-4 text-xl">
                 <span className="text-xl">Delhi:</span>
                 <div>A-10, Second Floor, Green Park, New Delhi, 110016</div>
               </div>
             </div>
 
-            <div className="lg:grid grid-cols-3 text-xl border-b border-gray-400">
-              <div className="lg:p-4 pt-5">Email ID:</div>
-              <Link  href="mailto:Info@upfrontglobal.com"><div className="lg:p-4 ">info@upfrontglobal.com</div></Link>
-            
+            <div className="lg:flex text-xl border-b border-gray-400">
+              <div className="lg:p-4 pt-5 pb-2 lg:pb-0">Email:</div>
+              <Link href="mailto:info@upfrontglobal.com">
+                <div className="lg:p-4 pt-5 pb-2">info@upfrontglobal.com</div>
+              </Link>
             </div>
 
             {/* <div className="lg:grid grid-cols-3 lg:gap-4 text-xl">
@@ -86,7 +83,7 @@ const Page = () => {
   const title = " Contact Us — Get in Touch with Upfront Today";
   const description =
     " Do you have any questions or need help? Reach out and connect with Upfront regarding our workforce well-being programs.";
-  const path = "https://upfront.global/contact-us"
+  const path = "https://upfront.global/contact-us";
   const metaImage = "/contactUs/banner.png";
 
   return (
@@ -100,7 +97,9 @@ const Page = () => {
         metaImage={metaImage}
       />
       <div className="lg:bg-[url('/contactUs/banner.png')] bg-[url('/contactUs/bannerMobile.png')] bg-cover lg:h-[80vh] h-[600px] flex flex-col pb-8">
-        <h1 className="text-white text-6xl mt-auto lg:ps-16 ps-10">CONTACT US</h1>
+        <h1 className="text-white text-6xl mt-auto lg:ps-16 ps-10">
+          CONTACT US
+        </h1>
       </div>
 
       <div className="lg:sticky top-24 bg-white shadow-md z-10">
